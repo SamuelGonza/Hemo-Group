0973770d567c74b1024e4dc4e18b241941fd4707
TecnoTics <144843516+tecnotics@users.noreply.github.com>
2024-02-05 09:57:04 -0500

    Commit Rutas

--- Hemo-Group/src/Components/Navbar/Navbar.jsx
+++ Hemo-Group/src/Components/Navbar/Navbar.jsx
@@ -10,6 +10,7 @@ export default function Navbar() {
       <div className="menu-list">
         <>
           <div className="menu">
+<<<<<<< Updated upstream
             <img
               className="logo1"
               src={logoSolo}
@@ -18,18 +19,31 @@ export default function Navbar() {
           </div>
           <div className="menu">
             <NavLink to="/quienessomos" className="frame">
+=======
+            <NavLink to="/" className="frame">
+              <i className="ri-home-3-line"></i>
+              <p className="li-mobile-p">Inicio</p>
+            </NavLink>
+          </div>
+          <div className="menu">
+            <NavLink to="/" className="frame">
+>>>>>>> Stashed changes
               <i className="ri-group-line"></i>
               <p className="li-mobile-p">Quienes Somos</p>
             </NavLink>
           </div>
           <div className="menu">
+<<<<<<< Updated upstream
             <NavLink to="/servicios" className="frame">
+=======
+            <NavLink to="/" className="frame">
+>>>>>>> Stashed changes
               <i className="ri-service-line"></i>
               <p className="li-mobile-p">Servicios</p>
             </NavLink>
           </div>
           <div className="menu">
-            <NavLink to="/estadistica" className="frame">
+            <NavLink to="/" className="frame">
               <i className="ri-handbag-line"></i>
               <p className="li-mobile-p">Directorio Medico</p>
             </NavLink>
@@ -42,13 +56,13 @@ export default function Navbar() {
             </div>
           </div>
           <div className="menu">
-            <NavLink to="/MiPerfil" className="frame">
+            <NavLink to="/" className="frame">
               <i className="ri-file-text-line "></i>
               <p className="li-mobile-p">Contacto</p>
             </NavLink>
           </div>
           <div className="menu">
-            <NavLink to="/MiPerfil" className="frame">
+            <NavLink to="/" className="frame">
               <i className="ri-file-info-line "></i>
               <p className="li-mobile-p">Informes</p>
             </NavLink>
